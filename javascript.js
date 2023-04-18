6f58b56be66fe305334f98b020b4f6781df5c758
gyubin1108 <130533797+gyubin1108@users.noreply.github.com>
2023-04-18 11:23:36 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,135 @@
-const display = document.querySelector(".display-screen");
-const buttons = document.querySelectorAll(".button");
+const display = document.querySelector('.calculator-display');
 
-buttons.forEach(button => {
-  button.addEventListener("click", calculate);
+const numberButtons = document.querySelectorAll('[data-number]');
+
+const operatorButtons = document.querySelectorAll('[data-operator]');
+
+const clearButton = document.querySelector('[data-clear]');
+
+const equalsButton = document.querySelector('[data-equals]');
+
+ 
+
+let currentValue = '';
+
+let previousValue = '';
+
+let currentOperator = null;
+
+let shouldResetDisplay = false;
+
+ 
+
+numberButtons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+if (shouldResetDisplay) {
+
+display.value = '';
+
+shouldResetDisplay = false;
+
+}
+
+display.value += button.textContent;
+
 });
 
-function calculate(event) {
-  const clickedButtonValue = event.target.value;
+});
 
-  if (clickedButtonValue === "=") {
-    if (display.value !== "") {
-      display.value = eval(display.value);
-    }
-  } else if (clickedButtonValue === "c") {
-    display.value = "";
-  } else {
-    display.value += clickedButtonValue;
-  }
+ 
+
+operatorButtons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+if (currentOperator) calculate();
+
+previousValue = display.value;
+
+currentOperator = button.dataset.operatorType;
+
+shouldResetDisplay = true;
+
+});
+
+});
+
+ 
+
+equalsButton.addEventListener('click', () => {
+
+if (!currentOperator) return;
+
+calculate();
+
+});
+
+ 
+
+clearButton.addEventListener('click', () => {
+
+currentValue = '';
+
+previousValue = '';
+
+currentOperator = null;
+
+display.value = '';
+
+});
+
+ 
+
+function calculate() {
+
+currentValue = display.value;
+
+let result;
+
+ 
+
+switch (currentOperator) {
+
+case 'add':
+
+result = parseFloat(previousValue) + parseFloat(currentValue);
+
+break;
+
+case 'subtract':
+
+result = parseFloat(previousValue) - parseFloat(currentValue);
+
+break;
+
+case 'multiply':
+
+result = parseFloat(previousValue) * parseFloat(currentValue);
+
+break;
+
+case 'divide':
+
+result = parseFloat(previousValue) / parseFloat(currentValue);
+
+break;
+
+default:
+
+return;
+
 }
+
+ 
+
+display.value = result;
+
+currentOperator = null;
+
+shouldResetDisplay = true;
+
+}
+
+ 
